7ee2f8e76cb75a0d658c6b2bef64b1d8963586b1
u68-backup <145247547+u68-backup@users.noreply.github.com>
2023-09-28 12:30:31 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ printf '#!/bin/bash\nfind />~/.findq/lib/library'>./buildlib
 chmod 777 ./buildlib
 mv ./buildlib /usr/local/bin/buildlib
 printf '#!/bin/bash\ncat ~/.findq/lib/library | grep $1'>./findq
-chmod 777 ./finq
+chmod 777 ./findq
 mv ./findq /usr/local/bin
 cd ~/
 printf 'Done!\nThat was quick...'
